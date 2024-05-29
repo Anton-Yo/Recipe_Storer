3ac94e34d1fdd65774857f0463da7c68f65add65
Anton-Yo <79824285+Anton-Yo@users.noreply.github.com>
2024-05-29 16:10:20 +1000

    Working on implementing DB with FastAPI
    - creating models.py

--- main.py
+++ main.py
@@ -11,5 +11,12 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hellow World"}
+def root():
+    return {"message": "Hello World"}\
+    
+@app.get("/test/{gg}")
+def get_test(gg: int):
+    if gg > 5:
+        return {"message": "sup ganstas"}
+    else:
+        return {"message": "u smol"}
--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,15 @@
-from flask import Flask
+# from flask import Flask
 
 
-def createApp():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = "gjojklsjkakljsdkljemncnmnmsjkjilasijoe"
+# def createApp():
+#     app = Flask(__name__)
+#     app.config['SECRET_KEY'] = "gjojklsjkakljsdkljemncnmnmsjkjilasijoe"
 
-    from .views import views
-    from .auth import auth
+#     from .views import views
+#     from .auth import auth
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+#     app.register_blueprint(views, url_prefix='/')
+#     app.register_blueprint(auth, url_prefix='/')
 
-    return app
+#     return app
 
