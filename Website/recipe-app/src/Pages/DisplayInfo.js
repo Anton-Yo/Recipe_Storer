cc8d44bc5e818c421634bf22d69c8dabc3c0c5a1
Anton-Yo <79824285+Anton-Yo@users.noreply.github.com>
2024-08-15 22:13:36 +1000

    There are multiple routes

--- Website/recipe-app/src/Pages/DisplayInfo.js
+++ Website/recipe-app/src/Pages/DisplayInfo.js
@@ -4,16 +4,63 @@ import '../App.css'
 
 
 const DisplayInfo = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [formData, setFormData] = useState({
+    name: "",
+    desc: "",
+    cuisine_name: "",
+    ingredients: [],
+    steps: [],
+  });
+
+  
+  useEffect(() => {
+    fetchRecipes();
+  }, []);
+
+  const fetchRecipes = async() => {
+    const response = await api.get("/recipes");
+    setRecipes(response.data)
+  };
 
   return (
-      <div>
-
-        <div className='container'>
-          <p> This is the display Page </p>
-        </div>
-
+    <div>
+      <div className="container">
+        <p> This is the display Page </p>
       </div>
-  )
+
+      <div className="mx-5 my-3 boxes-container">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="recipe-box">
+            <p> {recipe.name} </p>
+            <p> {recipe.desc} </p>
+          </div>
+        ))}
+      </div>
+
+
+      <table className='table table-striped table-bordered table-hover mt-3'>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Cuisine</th>
+          <th className="text-center">Delete?</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {recipes.map((recipe) => (
+          <tr key={recipe.id}>
+            <td>{recipe.name}</td>
+            <td>{recipe.desc}</td>
+            <td>{recipe.cuisine_id}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    </div>
+  );
 }
 
 export default DisplayInfo;
