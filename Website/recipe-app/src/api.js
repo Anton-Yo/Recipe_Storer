ed34d76a39bcf1ae5bf5277bb74bf403a4763a39
Anton-Yo <79824285+Anton-Yo@users.noreply.github.com>
2024-08-30 01:08:40 +1000

    The moment of truth :pray:

--- Website/recipe-app/src/api.js
+++ Website/recipe-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:  'http://127.0.0.1:8000/' 
+    baseURL:  'https://recipe-storer-1.onrender.com' 
    
     //baseURL: "https://recipe-storer-9mktzz8ml-anton-yos-projects.vercel.app/"
 })
