862e103f5b6bcba10dc2d6a20d3eea4bf88e691d
Anton-Yo <79824285+Anton-Yo@users.noreply.github.com>
2024-08-29 15:16:26 +1000

    Adding a secondary link?

--- Website/recipe-app/src/api.js
+++ Website/recipe-app/src/api.js
@@ -2,10 +2,7 @@ import axios from 'axios'
 
 const api = axios.create({
     //baseURL: "http://127.0.0.1:8000/"
-    //baseURL: window.location.origin
-    baseURL: process.env.REACT_APP_VERCEL_URL == null ? 'http://127.0.0.1:8000/' : `https://${process.env.REACT_APP_VERCEL_URL}`
-   
-    //baseURL: "https://recipe-storer-9mktzz8ml-anton-yos-projects.vercel.app/"
+    baseURL: process.env.REACT_APP_VERCEL_URL == null ? 'https://recipe-storer-git-main-anton-yos-projects.vercel.app/' : `https://${process.env.REACT_APP_VERCEL_URL}`
 })
 
 export default api;
