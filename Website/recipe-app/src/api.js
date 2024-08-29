8baf35ee3ea9458cbce81b01c229e5713678a74f
Anton-Yo <79824285+Anton-Yo@users.noreply.github.com>
2024-08-30 00:22:05 +1000

    Returning to previous build

--- Website/recipe-app/src/api.js
+++ Website/recipe-app/src/api.js
@@ -1,25 +1,9 @@
 import axios from 'axios'
 
-
-
-
 const api = axios.create({
-    //baseURL: "http://127.0.0.1:8000/"
-    baseURL: process.env.VERCEL_URL == null ? 'http://127.0.0.1:8000/' : `https://${process.env.VERCEL_URL}`
+    baseURL:  'http://127.0.0.1:8000/' 
    
     //baseURL: "https://recipe-storer-9mktzz8ml-anton-yos-projects.vercel.app/"
 })
-
-const getString = () =>
-{
-    if(process.env.VERCEL_URL == null)
-    {
-        return 'http://127.0.0.1:8000/'
-    }
-    else
-    {
-        return `https://${process.env.VERCEL_URL}`
-    }
-}
 
 export default api;
